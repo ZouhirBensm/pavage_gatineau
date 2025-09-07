aafd60c667b02c1f2a2d9432a90e4c9ec0c06c69
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-09-07 09:00:03 -0400

    Edit the emails to have 1 email

--- public/js/demande-de-devis-gratuit.js
+++ public/js/demande-de-devis-gratuit.js
@@ -1,49 +1,34 @@
-// console.log("business_data ->", business_data.email)
-
 function sendEmail(event) {
   event.preventDefault(); // Prevent the form from submitting normally
 
-
   const email_1 = 'pavagegatineau@outlook.com'; // mailto
-  const email_2 = 'pavagegatineau@gmail.com'; // cc
-
+  let optional_email_2 = undefined;
+  // optional_email_2 = 'pavagegatineau@gmail.com';
   let optional_email_3 = undefined;
-  optional_email_3 = 'entreprisesxpert@gmail.com'; // cc
+  // optional_email_3 = 'entreprisesxpert@gmail.com'; // cc
   let optional_email_4 = undefined;
   // optional_email_4 = 'earnanswers@outlook.com';
 
-
-  
-  // const email_1 = business_data.email;
-
-
   // Get form data
-  var name = document.getElementById('contact_name').value;
-  var email = document.getElementById('contact_email').value;
-  var phone = document.getElementById('contact_phone').value;
-  var message = document.getElementById('contact_message').value;
+  const name = document.getElementById('contact_name').value;
+  const email = document.getElementById('contact_email').value;
+  const phone = document.getElementById('contact_phone').value;
+  const message = document.getElementById('contact_message').value;
 
-
-  console.log(name, email)
-  // console.log(phone, message)
-
+  console.log(name, email);
 
   // Build the recipient list
-  let recipientList = email_1;  
-  let ccList = email_2;
+  const recipientList = email_1;
+  const ccEmails = [];
 
-  if (optional_email_3) {
-    ccList += ',' + optional_email_3;
-  }
-
-  if (optional_email_4) {
-    ccList += ',' + optional_email_4;
-  }
+  // Add optional emails to ccEmails array if they are defined
+  if (optional_email_2) ccEmails.push(optional_email_2);
+  if (optional_email_3) ccEmails.push(optional_email_3);
+  if (optional_email_4) ccEmails.push(optional_email_4);
 
   // Construct the mailto link
-  var mailtoLink = 'mailto:' + recipientList +
-    '?cc=' + encodeURIComponent(ccList) +
-    '&subject=' + encodeURIComponent('Pavage Gatineau: Quote Request') +
+  let mailtoLink = 'mailto:' + recipientList +
+    '?subject=' + encodeURIComponent('Pavage Gatineau: Quote Request') +
     '&body=' + encodeURIComponent(
       'Name: ' + name + '\n' +
       'Phone: ' + phone + '\n' +
@@ -51,12 +36,15 @@ function sendEmail(event) {
       'Message: ' + message
     );
 
+  // Only add CC if there are any CC emails
+  if (ccEmails.length > 0) {
+    mailtoLink = mailtoLink.replace('?', '?cc=' + encodeURIComponent(ccEmails.join(',')) + '&');
+  }
+
   // Open the mailto link in the default email client
   window.location.href = mailtoLink;
-  return
 }
 
-
 const form = document.getElementById("tmContactForm");
-console.log(form)
+console.log(form);
 form.addEventListener("submit", sendEmail);
